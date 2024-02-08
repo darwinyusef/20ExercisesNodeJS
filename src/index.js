ed327b03952c109e4fcfc9afbb0520a2a5cae3fa
Yusef Gonzalez <yusef@tresastronautas.com>
2024-02-08 11:03:48 -0500

    Revisión de conexiones usando socket

--- src/index.js
+++ src/index.js
@@ -13,11 +13,25 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 app.use(express.static(path.join(__dirname, '/views')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log({id: socket.id});
+io.on("connection", socket => {
+
+    console.log("Clientes conectados: ", io.engine.clientsCount);
+    console.log("ID del socket conectado: ", socket.id);
+    socket.on("hello", (arg) => {
+        console.log(`Alguien dijo hola ${arg}`);   
+    });
+
+    // socket.on("disconnect", () => {
+    //     console.log("El socket " + socket.id + " se ha desconectado.");
+    // });
+
+    // socket.conn.once("upgrade", () => {
+    //     console.log("Hemos pasado de HTTP Long-Polling a ", socket.conn.transport.name);
+    // });
+
 });
 
 httpServer.listen(PORT, function () {
